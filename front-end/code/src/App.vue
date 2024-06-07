1a6560dd9a0183048c74965ca7313fbb951a7e34
Leonid Danilov <leonid989@gmail.com>
2024-06-07 14:22:20 +0300

    Split into 2

--- front-end/code/src/App.vue
+++ front-end/code/src/App.vue
@@ -1,10 +1,82 @@
 <script setup lang="ts">
 import AutoComplete from './components/AutoComplete.vue'
+import { useCitiesSearchStore, useBooksSearchStore } from './stores/store'
+
+function getData() {
+  const cities = [
+    'san jose',
+    'santiago',
+    'san francisco',
+    'santa rosa',
+    'san juan',
+    'sabadell',
+    'salamanca',
+    'salt lake city',
+    'salinas',
+    'salem',
+    'sausalito',
+    'taipei',
+    'tel aviv',
+    'tempe',
+    'termez',
+    'temuco',
+    'tiajuna',
+    'tieling',
+    'thousand oaks',
+    'thunder bay',
+    'tokyo',
+    'tulsa'
+  ]
+
+  const books = [
+    {
+      title: 'Don Quixote',
+      author: 'Miguel De Cervantes'
+    },
+    {
+      title: "Pilgrim's Progress",
+      author: 'John Bunyan'
+    },
+    {
+      title: 'Robinson Crusoe',
+      author: 'Daniel Defoe'
+    },
+    {
+      title: "Gulliver's Travels",
+      author: 'Jonathan Swift'
+    },
+    {
+      title: 'Tom Jones',
+      author: 'Henry Fielding'
+    },
+    {
+      title: 'Clarissa',
+      author: 'Samuel Richardson'
+    },
+    {
+      title: 'Tristram Shandy',
+      author: 'Laurence Sterne'
+    }
+  ]
+
+  return { cities, books }
+}
+
+const citiesData = getData().cities
+const booksData = getData().books.map((book) => `${book.title} by ${book.author}`)
+
+const citiesStore = useCitiesSearchStore()
+const booksStore = useBooksSearchStore()
 </script>
 
 <template>
   <main class="main">
-    <AutoComplete />
+    <div>
+      <AutoComplete ype="cities" label="Cities" :data="citiesData" :store="citiesStore" />
+    </div>
+    <div>
+      <AutoComplete type="books" label="Books" :data="booksData" :store="booksStore" />
+    </div>
   </main>
 </template>
 
--- front-end/code/src/components/AutoComplete.vue
+++ front-end/code/src/components/AutoComplete.vue
@@ -1,70 +1,9 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import { useCitiesSearchStore, useBooksSearchStore } from '../stores/store'
 
-const citiesStore = useCitiesSearchStore()
-const booksStore = useBooksSearchStore()
+const props = defineProps(['type', 'label', 'store', 'data'])
 
-function getData() {
-  const cities = [
-    'san jose',
-    'santiago',
-    'san francisco',
-    'santa rosa',
-    'san juan',
-    'sabadell',
-    'salamanca',
-    'salt lake city',
-    'salinas',
-    'salem',
-    'sausalito',
-    'taipei',
-    'tel aviv',
-    'tempe',
-    'termez',
-    'temuco',
-    'tiajuna',
-    'tieling',
-    'thousand oaks',
-    'thunder bay',
-    'tokyo',
-    'tulsa'
-  ]
-
-  const books = [
-    {
-      title: 'Don Quixote',
-      author: 'Miguel De Cervantes'
-    },
-    {
-      title: "Pilgrim's Progress",
-      author: 'John Bunyan'
-    },
-    {
-      title: 'Robinson Crusoe',
-      author: 'Daniel Defoe'
-    },
-    {
-      title: "Gulliver's Travels",
-      author: 'Jonathan Swift'
-    },
-    {
-      title: 'Tom Jones',
-      author: 'Henry Fielding'
-    },
-    {
-      title: 'Clarissa',
-      author: 'Samuel Richardson'
-    },
-    {
-      title: 'Tristram Shandy',
-      author: 'Laurence Sterne'
-    }
-  ]
-
-  return { cities, books }
-}
-
+const suggestionsId = `suggestions-${props.type}`
 const suggestions = ref([])
 
 function isContainingString(mainString: string, subString: string): boolean {
@@ -74,36 +13,31 @@ function isContainingString(mainString: string, subString: string): boolean {
 function getSuggestions(rawSearchValue: string): string[] {
   const searchValue = rawSearchValue.toLowerCase().trim()
 
-  const { cities, books } = getData()
-
-  const filteredCities = cities.filter((city) => isContainingString(city, searchValue))
-  const filteredBooks = books.filter((book) => isContainingString(book.title, searchValue))
-
-  return [...filteredCities, ...filteredBooks]
+  return props.data.filter((value) => isContainingString(value, searchValue))
 }
 
 function handleChange(event: InputEvent) {
   const newValue = event.target.value
-  searchStore.setSearch(newValue)
+  props.store.setSearch(newValue)
   suggestions.value = getSuggestions(newValue)
 }
 
-const showCitiesSuggestions = computed(() => citiesStore.searchValue.length > 3)
+const showSuggestions = computed(() => props.store.searchValue.length > 3)
 </script>
 
 <template>
   <div class="wrapper">
     <div class="label-row">
-      <label class="label" for="searchbar">You know what to do:</label>
+      <label class="label" for="searchbar">{{ props.label }}:</label>
     </div>
     <input
       autofocus
       id="searchbar"
       @input="handleChange"
-      :value="searchStore.searchValue"
-      list="cities-suggestions"
+      :value="props.store.searchValue"
+      :list="suggestionsId"
     />
-    <datalist v-if="showCitiesSuggestions" id="cities-suggestions">
+    <datalist v-if="showSuggestions" :id="suggestionsId">
       <option v-for="suggestion in suggestions" :key="suggestion" :value="suggestion" />
     </datalist>
   </div>
