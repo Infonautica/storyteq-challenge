8d680e31be660cd0b2c36da2db78b560a162a497
Leonid Danilov <leonid989@gmail.com>
2024-06-09 21:11:59 +0300

    Chores

--- front-end/code/README.md
+++ front-end/code/README.md
@@ -13,14 +13,14 @@ The project is based on vue-start and includes the following features:
 ## My notes
 
 The world is complex enough, so i personally started to lean towards simplicity.
-My vision of the world will obviously be reflected in my code, so you may find things like:
+It then starts to be reflected in my code, so you may find things like:
 
 - A simple and minimalistic UI
 - HTML based autofocus instead of JS one
 - Datalist with suggestions instead of custom solutions
 - Single purpose functions
 - No unnecessary dependencies
-- Clear and simple naming concentions
+- Clear and simple naming conventions
 - etc.
 
 ## CLI Instructions
--- front-end/code/src/App.vue
+++ front-end/code/src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import AutoComplete from './components/AutoComplete.vue'
 import { useCitiesSearchStore, useBooksSearchStore } from './stores/store'
+import { getData } from './data.ts'
 import { Application } from '@splinetool/runtime'
 
 const canvas = document.getElementById('bg-canvas') as HTMLCanvasElement
@@ -17,66 +18,6 @@ function handleClickCompany() {
   const logo = spline.findObjectByName('logo')
   logo.emitEvent('mouseDown')
   hasClicked.value = true
-}
-
-function getData() {
-  const cities = [
-    'san jose',
-    'santiago',
-    'san francisco',
-    'santa rosa',
-    'san juan',
-    'sabadell',
-    'salamanca',
-    'salt lake city',
-    'salinas',
-    'salem',
-    'sausalito',
-    'taipei',
-    'tel aviv',
-    'tempe',
-    'termez',
-    'temuco',
-    'tiajuna',
-    'tieling',
-    'thousand oaks',
-    'thunder bay',
-    'tokyo',
-    'tulsa'
-  ]
-
-  const books = [
-    {
-      title: 'Don Quixote',
-      author: 'Miguel De Cervantes'
-    },
-    {
-      title: "Pilgrim's Progress",
-      author: 'John Bunyan'
-    },
-    {
-      title: 'Robinson Crusoe',
-      author: 'Daniel Defoe'
-    },
-    {
-      title: "Gulliver's Travels",
-      author: 'Jonathan Swift'
-    },
-    {
-      title: 'Tom Jones',
-      author: 'Henry Fielding'
-    },
-    {
-      title: 'Clarissa',
-      author: 'Samuel Richardson'
-    },
-    {
-      title: 'Tristram Shandy',
-      author: 'Laurence Sterne'
-    }
-  ]
-
-  return { cities, books }
 }
 
 const citiesData = getData().cities
